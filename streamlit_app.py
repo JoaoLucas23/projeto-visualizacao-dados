a33e03d4808323554ccaf8430a951331780a54f1
Kleber Junior <kleber.alpe@gmail.com>
2024-06-13 22:15:38 -0300

    Adding new visualization - Atividades Emissoras

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ import pandas as pd
 
 from tabs.emissaoCO2 import draw_tab2
 from tabs.temperatura import draw_tab3
-from tabs.anoArea import draw_tab4
-from tabs.prodAgricola import draw_tab1
+from tabs.atividadesEmissoras import draw_tab1
 from tabs.intro import draw_intro
 
 st.title('Projeto de Visualização de Dados')
@@ -12,7 +11,7 @@ st.title('Projeto de Visualização de Dados')
 raw_df = pd.read_csv('./data/Agrofood_co2_emission.csv')
 
 
-intro, tab1, tab2, tab3, tab4= st.tabs(["Intro","Produção Agrícola","Emissões de CO2", "Temperatura", "Ano x Area"])
+intro, tab1, tab2, tab3= st.tabs(["Intro","Atividades Emissoras","Emissões de CO2", "Temperatura"])
 with intro:
     draw_intro()
 
@@ -24,6 +23,3 @@ with tab2:
 
 with tab3:
     draw_tab3(raw_df)
-
-with tab4:
-    draw_tab4(raw_df)
---
+++ tabs/atividadesEmissoras.py
@@ -0,0 +1,277 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from sklearn.preprocessing import MinMaxScaler
+import pycountry
+
+def select_columns(df):
+    selected_columns = ['Area', 'total_emission', 'Agrifood Systems Waste Disposal', 'Food Household Consumption', 'Forest fires', 'Fires in humid tropical forests', 'Savanna fires', 'IPPU', 'Food Processing', 'Food Transport', 'Food Packaging', 'Pesticides Manufacturing', 'Fertilizers Manufacturing', 'Food Retail', 'Rice Cultivation', 'Manure left on Pasture', 'Drained organic soils (CO2)', 'Crop Residues', 'Manure Management', 'Manure applied to Soils', 'Average Temperature °C', 'Year']
+    return df[selected_columns].copy()
+
+def calculate_total_emissions(df):
+    emission_sources = ['Forest fires', 'Fires in humid tropical forests', 'Savanna fires', 'IPPU', 'Food Processing', 'Food Transport', 'Food Packaging', 'Pesticides Manufacturing', 'Fertilizers Manufacturing', 'Food Retail', 'Rice Cultivation', 'Manure left on Pasture', 'Drained organic soils (CO2)', 'Crop Residues', 'Manure Management', 'Manure applied to Soils']
+
+    df['total_fire_emissions'] = df[emission_sources[:3]].sum(axis=1)
+    df['total_industrial_emissions'] = df[emission_sources[3:12]].sum(axis=1)
+    df['total_cultivation_emissions'] = df[emission_sources[12:]].sum(axis=1)
+    return df
+
+def get_iso_alpha(country_name):
+    try:
+        country = pycountry.countries.get(name=country_name)
+        iso_alpha = country.alpha_3
+        return iso_alpha
+    except:
+        return None
+
+def chart1(df):
+    df_copia = df.copy()
+    CO2_df = df[['Area', 'total_emission', 'Average Temperature °C']]
+    mean_CO2_df = CO2_df.groupby('Area').mean()
+
+    scaler = MinMaxScaler()
+    mean_CO2 = scaler.fit_transform(mean_CO2_df)
+    normalized_emission = pd.DataFrame(mean_CO2, columns=['mean_CO2_emission', 'Average Temperature °C'], index=mean_CO2_df.index)
+    normalized_emission['Area'] = normalized_emission.index
+    normalized_emission['iso_alpha'] = normalized_emission['Area'].apply(get_iso_alpha)
+
+    fig = px.choropleth \
+    (
+        normalized_emission,
+        locations="iso_alpha",
+        color="mean_CO2_emission",
+        hover_name="Area",
+        color_continuous_scale=["#FA8072", "#FF0000"] 
+    )
+
+    fig2 = px.scatter_geo \
+    (
+        normalized_emission,
+        locations="iso_alpha",
+        size="Average Temperature °C"
+    )
+
+    fig.add_trace(fig2.data[0])
+
+    fig.update_layout(
+        title={'text': "Emissões médias de CO2 x Aumento médio da temperatura por país", 'x': 0.5, 'xanchor': 'center'},
+        autosize=False,
+        height=600,
+        width=1200
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
+
+def chart2(df):
+    temp = select_columns(df)
+    temp = select_columns(df)
+    temp = calculate_total_emissions(temp)
+
+    CO2_df = temp[['Area', 'total_emission', 'total_industrial_emissions']]
+    mean_CO2_df = CO2_df.groupby('Area').mean()
+
+    scaler = MinMaxScaler()
+    mean_CO2 = scaler.fit_transform(mean_CO2_df)
+    normalized_emission = pd.DataFrame(mean_CO2, columns=['mean_CO2_emission', 'mean_industrial_emissions'], index=mean_CO2_df.index)
+    normalized_emission['Area'] = normalized_emission.index
+    normalized_emission['iso_alpha'] = normalized_emission['Area'].apply(get_iso_alpha)
+    normalized_emission['mean_industrial_emissions'] = normalized_emission['mean_industrial_emissions'].fillna(0)
+
+    fig = px.choropleth \
+    (
+        normalized_emission,
+        locations="iso_alpha",
+        color="mean_CO2_emission",
+        hover_name="Area",
+        color_continuous_scale=["#FA8072", "#FF0000"] 
+    )
+
+    fig2 = px.scatter_geo \
+    (
+        normalized_emission,
+        locations="iso_alpha",
+        size="mean_industrial_emissions"
+    )
+
+    fig.add_trace(fig2.data[0])
+
+    fig.update_layout(
+        title={'text': "Emissões médias de CO2 x Emissões industriais médias por país", 'x': 0.5, 'xanchor': 'center'},
+        autosize=False,
+        height=600,
+        width=1200
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
+
+def chart3(df):
+    temp = select_columns(df)
+    temp = calculate_total_emissions(temp)
+
+    CO2_df = temp[['Area', 'total_emission', 'Agrifood Systems Waste Disposal']]
+    mean_CO2_df = CO2_df.groupby('Area').mean()
+
+    scaler = MinMaxScaler()
+    mean_CO2 = scaler.fit_transform(mean_CO2_df)
+    normalized_emission = pd.DataFrame(mean_CO2, columns=['mean_CO2_emission', 'mean_waste_emission'], index=mean_CO2_df.index)
+    normalized_emission['Area'] = normalized_emission.index
+    normalized_emission['iso_alpha'] = normalized_emission['Area'].apply(get_iso_alpha)
+    normalized_emission['mean_waste_emission'] = normalized_emission['mean_waste_emission'].fillna(0)
+
+    fig = px.choropleth(
+        normalized_emission,
+        locations="iso_alpha",
+        color="mean_CO2_emission",
+        hover_name="Area",
+        color_continuous_scale=["#FA8072", "#FF0000"] 
+    )
+
+    fig2 = px.scatter_geo \
+    (
+        normalized_emission,
+        locations="iso_alpha",
+        size="mean_waste_emission"
+    )
+
+    fig.add_trace(fig2.data[0])
+
+    fig.update_layout \
+    (
+        title={'text': "Emissões médias de CO2 x Emissões de eliminação de resíduos agroalimentares por país", 'x': 0.5, 'xanchor': 'center'},
+        autosize=False,
+        height=600,
+        width=1200
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
+
+def chart4(df):
+    temp = select_columns(df)
+    temp = calculate_total_emissions(temp)
+
+    CO2_df = temp[['Area', 'total_emission', 'total_cultivation_emissions']]
+    mean_CO2_df = CO2_df.groupby('Area').mean()
+
+    scaler = MinMaxScaler()
+    mean_CO2 = scaler.fit_transform(mean_CO2_df)
+    normalized_emission = pd.DataFrame(mean_CO2, columns=['mean_CO2_emission', 'mean_cultivation_emissions'], index=mean_CO2_df.index)
+    normalized_emission['Area'] = normalized_emission.index
+    normalized_emission['iso_alpha'] = normalized_emission['Area'].apply(get_iso_alpha)
+    normalized_emission['mean_cultivation_emissions'] = normalized_emission['mean_cultivation_emissions'].fillna(0)
+
+    fig = px.choropleth(
+        normalized_emission,
+        locations="iso_alpha",
+        color="mean_CO2_emission",
+        hover_name="Area",
+        color_continuous_scale=["#FA8072", "#FF0000"] 
+    )
+
+    fig2 = px.scatter_geo \
+    (
+        normalized_emission,
+        locations="iso_alpha",
+        size="mean_cultivation_emissions"
+    )
+
+    fig.add_trace(fig2.data[0])
+
+    fig.update_layout \
+    (
+        title={'text': "Emissões médias de CO2 x Emissões médias de cultivo por país", 'x': 0.5, 'xanchor': 'center'},
+        autosize=False,
+        height=600,
+        width=1200
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
+
+def chart5(df):
+    temp = select_columns(df)
+    temp = calculate_total_emissions(temp)
+
+    CO2_df = temp[['Area', 'total_emission', 'total_fire_emissions']]
+    mean_CO2_df = CO2_df.groupby('Area').mean()
+
+    scaler = MinMaxScaler()
+    mean_CO2 = scaler.fit_transform(mean_CO2_df)
+    normalized_emission = pd.DataFrame(mean_CO2, columns=['mean_CO2_emission', 'mean_fire_emissions'], index=mean_CO2_df.index)
+    normalized_emission['Area'] = normalized_emission.index
+    normalized_emission['iso_alpha'] = normalized_emission['Area'].apply(get_iso_alpha)
+    normalized_emission['mean_fire_emissions'] = normalized_emission['mean_fire_emissions'].fillna(0)
+
+    fig = px.choropleth(
+        normalized_emission,
+        locations="iso_alpha",
+        color="mean_CO2_emission",
+        hover_name="Area",
+        color_continuous_scale=["#FA8072", "#FF0000"] 
+    )
+
+    fig2 = px.scatter_geo \
+    (
+        normalized_emission,
+        locations="iso_alpha",
+        size="mean_fire_emissions"
+    )
+
+    fig.add_trace(fig2.data[0])
+
+    fig.update_layout \
+    (
+        title={'text': "Emissões médias de CO2 x Emissões de incêndios por país", 'x': 0.5, 'xanchor': 'center'},
+        autosize=False,
+        height=600,
+        width=1200
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
+
+def chart6(df):
+    temp = select_columns(df)
+    temp = calculate_total_emissions(temp)
+
+    CO2_df = temp[['Area', 'total_emission', 'Food Household Consumption']]
+    mean_CO2_df = CO2_df.groupby('Area').mean()
+
+    scaler = MinMaxScaler()
+    mean_CO2 = scaler.fit_transform(mean_CO2_df)
+    normalized_emission = pd.DataFrame(mean_CO2, columns=['mean_CO2_emission', 'mean_household_emission'], index=mean_CO2_df.index)
+    normalized_emission['Area'] = normalized_emission.index
+    normalized_emission['iso_alpha'] = normalized_emission['Area'].apply(get_iso_alpha)
+    normalized_emission['mean_household_emission'] = normalized_emission['mean_household_emission'].fillna(0)
+
+    fig = px.choropleth(
+        normalized_emission,
+        locations="iso_alpha",
+        color="mean_CO2_emission",
+        hover_name="Area",
+        color_continuous_scale=["#FA8072", "#FF0000"] 
+    )
+
+    fig2 = px.scatter_geo \
+    (
+        normalized_emission,
+        locations="iso_alpha",
+        size="mean_household_emission"
+    )
+
+    fig.add_trace(fig2.data[0])
+
+    fig.update_layout \
+    (
+        title={'text': "Emissões médias de CO2 x Emissões de consumo doméstico por país", 'x': 0.5, 'xanchor': 'center'},
+        autosize=False,
+        height=600,
+        width=1200
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
+
+def draw_tab1(df):
+    chart1(df)
+    chart2(df)
+    chart3(df)
+    chart4(df)
+    chart5(df)
+    chart6(df)
